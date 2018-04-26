1fa6e0d8a6065268f9dc09682a7cf19c524caac9
facuobregon <facuobregon@gmail.com>
2018-04-26 17:05:00 -0300

    Add project files.

---
+++ Move2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Move2._0.Startup))]
+namespace Move2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
