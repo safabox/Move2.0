1419d32be1042ac0053ab187d813d1dbeb98ebda
facuobregon <facuobregon@gmail.com>
2018-04-26 20:31:16 -0300

    restapi de preguntas y respuestas

---
+++ Move2.0/Controllers/Api/PreguntasController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Move2._0.DAL;
+using Move2._0.Models;
+
+namespace Move2._0.Controllers.Api
+{
+    public class PreguntasController : ApiController
+    {
+
+        private ApplicationDbContext _context = null;
+
+        public PreguntasController() {
+            _context = ApplicationDbContext.Create();
+        }
+        protected override void Dispose(bool disposing)
+
+        {
+            _context.Dispose();
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Pregunta> GetPreguntas() {
+            var preguntas = _context.Pregunta.ToList();
+            return preguntas;
+
+        }
+        [HttpGet]
+        public Pregunta GetPregunta(int Id) {
+            var pregunta = _context.Pregunta.SingleOrDefault(c => c.Id == Id);
+            return pregunta;
+        }
+
+
+        [HttpGet]
+        [Route("api/preguntas/{Id}/respuestas")]
+        public IEnumerable<Respuesta> GetRespuestas(int Id) {
+            var respuestas = _context.Respuesta.Where(c => c.PreguntaId == Id).ToList();
+            return respuestas;
+        }
+
+    }
+}
--- Move2.0/Global.asax.cs
+++ Move2.0/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace Move2._0
 {
@@ -12,6 +14,20 @@ namespace Move2._0
     {
         protected void Application_Start()
         {
+
+
+            #region webApi
+
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+    .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            #endregion
+
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
